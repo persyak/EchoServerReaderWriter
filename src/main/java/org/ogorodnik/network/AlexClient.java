3b2f2033ae6d5338bf4c9df8e7a4d5b7dce31ac8
alexone <persyak@ukr.net>
2021-11-11 07:30:27 +0200

    refactored code according to review from Video. Moved \n to constant, removed while conditionin server and replaced it with while(true)

--- src/main/java/org/ogorodnik/network/AlexClient.java
+++ src/main/java/org/ogorodnik/network/AlexClient.java
@@ -4,6 +4,8 @@ import java.io.*;
 import java.net.Socket;
 
 public class AlexClient {
+    private final static String LINE_END = "\n";
+
     public static void main(String[] args) throws IOException {
         Socket socket = new Socket("127.0.0.1", 3000);
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
@@ -12,7 +14,8 @@ public class AlexClient {
             System.out.println("Insert word or phrase from a keyboard");
             String input;
             while (!("finish".equals(input = readerIn.readLine().toLowerCase()))) {
-                writer.write(input + '\n');
+                writer.write(input);
+                writer.write(LINE_END);
                 writer.flush();
                 System.out.println(reader.readLine());
                 System.out.println("Insert another word or phrase from a keyboard or word 'finish' to end connection");
--- src/main/java/org/ogorodnik/network/Server.java
+++ src/main/java/org/ogorodnik/network/Server.java
@@ -5,21 +5,26 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
+    private final static String LINE_END = "\n";
+
     public static void main(String[] args) throws IOException {
-        ServerSocket serverSocket = new ServerSocket(3000);
-        //listen
-        while (true) {
-            try (Socket socket = serverSocket.accept();
-                 BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                 BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()))) {
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    String messageFromClient = "Echo " + line;
-                    writer.write(messageFromClient + "\n");
-                    writer.flush();
+        try (ServerSocket serverSocket = new ServerSocket(3000)) {
+
+            //listen
+            while (true) {
+                try (Socket socket = serverSocket.accept();
+                     BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                     BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()))) {
+                    while (true) {
+                        String line = reader.readLine();
+                        String messageFromClient = "Echo " + line;
+                        writer.write(messageFromClient);
+                        writer.write(LINE_END);
+                        writer.flush();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
-            } catch (IOException e) {
-                e.printStackTrace();
             }
         }
     }
